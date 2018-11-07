5f9a91ebcd3052150d2adbbc2da33059506e61ac
PTuLo <pturriagol@gmail.com>
2018-11-07 16:37:49 -0400

    added what we need to do for each method

--- src/MP.cpp
+++ src/MP.cpp
@@ -9,7 +9,7 @@ MotionPlanner::MotionPlanner(Simulator * const simulator)
 
     Vertex *vinit = new Vertex();
 
-    vinit->m_parent   = -1;   
+    vinit->m_parent   = -1;		
     vinit->m_nchildren= 0;    
     vinit->m_state[0] = m_simulator->GetRobotCenterX();
     vinit->m_state[1] = m_simulator->GetRobotCenterY();
@@ -33,6 +33,12 @@ void MotionPlanner::ExtendTree(const int    vid,
 			       const double sto[])
 {
 //your code
+	//make a small part (size of step) of the line from the vertex from m_vertices[vid] to the point sto
+	//effectively you're just getting the vector made by the x and y coordinates of the vertex and sto
+	//then you're normalizing it and scaling it by the step size.
+	//check if its a valid point by runnning through the path to the new vertices if it is valid add it to the tree 
+	//if it isnt then dont add it and move on.
+	//could potentially just run from the vertex to the new point made after you normalized and scaled but unsure
 }
 
 void MotionPlanner::ExtendRandom(void)
@@ -41,6 +47,10 @@ void MotionPlanner::ExtendRandom(void)
     StartTime(&clk);
 
 //your code
+	//sto is chosen based on some probbability from the goal region using samplestate  
+	//vid is chosen randomly from the size of the m_vertices list (its the index)
+	//run the ExtendTree function with the new STO and Vertices
+	//check if youre within the goal region ti end it and print the solved time
     
     m_totalSolveTime += ElapsedTime(&clk);
 }
@@ -51,6 +61,10 @@ void MotionPlanner::ExtendRRT(void)
     StartTime(&clk);
  
 //your code
+	//sto is chosen the same way as above 
+	//vid is chosen based on the closest vertex  so we'll have to run through the vertices and find the distance 
+	//and keep track of the largest one as we go. potential to optimize by using a good sort but meh
+	//check done as above
     
     m_totalSolveTime += ElapsedTime(&clk);
 }
@@ -62,6 +76,11 @@ void MotionPlanner::ExtendEST(void)
     StartTime(&clk);
 
 //your code    
+	//sto is chosen as above
+	//vid chosen based on with probability proportional to how many children are coming out of it. 
+	//we know how much each vertex has (m_nchildren) but im unsure how to make it chose based on that since its a probability distribution 
+	//so someone else is gonna have think of something
+	//check done as above
     m_totalSolveTime += ElapsedTime(&clk);
 }
 
@@ -72,8 +91,11 @@ void MotionPlanner::ExtendMyApproach(void)
     StartTime(&clk);
  
 //your code
-    
-    m_totalSolveTime += ElapsedTime(&clk);
+    //sto chosen as above
+	//vid chosen from the sorting the vertex list by how close it is (we should build a helper function) and taking the
+	//5 (or some other number) closest ones and choseing one of those randomly (or we can do it based on how many children it has)
+    //check done as above
+	m_totalSolveTime += ElapsedTime(&clk);
 }
 
 
